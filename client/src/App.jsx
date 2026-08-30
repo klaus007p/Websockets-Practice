e8c2ea1e7b3b7c6b69b9f81a9e06bf4a7c3e1fe1
Prabhat Pathak <klaus007p@gmail.com>
2026-08-30 12:09:46 +0530

    done client/server side rendering of msgs used emit, broadcast to deliver the msgs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,31 @@
 import { useEffect } from 'react';
 import { useState } from 'react'
 import { io } from 'socket.io-client'
-
+import { Container, TextField, Typography } from '@mui/material';
+import { useMemo } from 'react';
 
 function App() {
   // const [count, setCount] = useState(0)
-  const socket = io("http://localhost:3000");
+  const socket = useMemo(() => io("http://localhost:3000"),[]);
+
+
+  const [message, setMessage] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("message", message);
+    
+  };
+
 
   useEffect(() => {
     socket.on("connect", () => {
       console.log("Connected", socket.id);
+    })
+
+    socket.on("recieve message", (data) => {
+      console.log(data);
+      
     })
 
     socket.on("welcome", (s) => {
@@ -17,13 +33,24 @@ function App() {
       
     })
 
+    // return () => {               // useeffect fires a callback when component unmount and it trigger a cleanup function 
+    //   socket.disconnect();
+    // }
   },[])
 
 
   return (
-    <>
-      <div>Hello sir How are you doing! </div>
-    </>
+    <Container maxWidth='sm'>
+      <Typography variant='h1' component="div" gutterBottom>
+        Welcome to the Socketio App
+      </Typography>
+
+      <form onSubmit={handleSubmit}>
+          <TextField value={message} onChange={(e)=> setMessage(e.target.value)} id='outlined-basic' label='Outlined' variant='outlined'/>
+          <button type='submit' variant='contained' color='primary'> Send </button>
+      </form>
+
+    </Container>
   )
 }
 
